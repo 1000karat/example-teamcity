c861b10d0371b2623ef14c3a98cdeb9323729b22
odmin <odmin@TeamCity>
2026-07-30 16:01:48 +0000

    triggers of 'Build' build configuration were updated (TeamCity change in 'exTeamcity' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -59,6 +60,11 @@ object Build : BuildType({
             goals = "deploy -DskipTests"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             userSettingsSelection = "settings.xml"
+        }
+    }
+
+    triggers {
+        vcs {
         }
     }
 })
